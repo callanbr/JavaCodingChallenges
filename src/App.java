497a69aba253d9bc22c5d8069a9ab4d02c1c7ac9
callanbr <chrisallan1992@icloud.com>
2020-10-29 07:40:20 -0700

    added Intermediate 1-3

--- src/App.java
+++ src/App.java
@@ -1,8 +1,7 @@
 import Starter.*;
+import Intermediate.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] arr = { 1, 1, 3, 7, 7, 8, 1, 1, 7 };
-        System.out.println(Starter.CountInArrX(arr));
     }
 }
---
+++ src/Intermediate/Intermediate.java
@@ -0,0 +1,30 @@
+package Intermediate;
+
+public class Intermediate {
+
+    // #1 - RockPaperScissor
+    // You have two players each can choose Rock, Paper, or Scissor.
+    // Return the winner- Rock wins Scissor, Paper wins Rock, Scissor wins Paper
+
+    public static String RockPaperScissor(String Player1, String Player2) {
+        return "";
+    }
+
+    // #2 - ReverseInt
+    // Create a function that takes an integer n and reverses it.
+    // abcdefg = > gfedcba
+
+    public static String ReverseInt(int n) {
+        return "";
+    }
+
+    // #3 Factorial
+    // 0! || 1! = 1
+    // 3! = 3 * 2 * 1 = 6
+    // 4! = 4 * 3 * 2 * 1 = 24
+
+    public static int Factorial(int n) {
+        return 0;
+    }
+
+}
